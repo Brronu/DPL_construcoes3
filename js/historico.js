4704a8cef7f557ba506a94753af40897f0490d69
Bruno Oliveira <brunooliveira.2115b@gmail.com>
2023-11-24 20:02:33 -0300

    iniciado

--- js/historico.js
+++ js/historico.js
@@ -1,5 +1,7 @@
 import { db } from "./config-firebase.js";
-import { getDocs, collection } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-firestore.js";
+import { orderBy, getDocs, collection, query } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-firestore.js";
+
+
 
 document.addEventListener("DOMContentLoaded", async () => {
     try {
@@ -25,12 +27,17 @@ function criarCardInformacaoHTML(data, ordemServico, timestamp) {
     let imagensHTML = "";
 
     // Verifica se o documento possui URLs das seis imagens
-    if (data.imgAterrar && data.imgBloquear && data.imgDesligar && data.imgProteger && data.imgSinalizar && data.imgTestar) {
-        const imagens = [data.imgAterrar, data.imgBloquear, data.imgDesligar, data.imgProteger, data.imgSinalizar, data.imgTestar];
+    const nomesImagens = ["APR", "ATERRAR", "BLOQUEAR", "DESLIGAR", "PROTEGER", "SINALIZAR", "TESTAR"];
 
-        imagens.forEach((imagem) => {
+    // Verifica se o documento possui URLs das sete imagens
+    if (data.imgApr && data.imgAterrar && data.imgBloquear && data.imgDesligar && data.imgProteger && data.imgSinalizar && data.imgTestar ) {
+        const imagens = [data.imgApr, data.imgAterrar, data.imgBloquear, data.imgDesligar, data.imgProteger, data.imgSinalizar, data.imgTestar];
+
+        // Itera sobre as imagens e adiciona ao HTML
+        imagens.forEach((imagem, index) => {
             imagensHTML += `
                 <div>
+                    <p class="fw-bolder fonte">${nomesImagens[index]}:</p>
                     <img src="${imagem.url}" alt="${imagem.nome}" style="max-width: 200px; max-height: 200px; margin-bottom: 10px;">
                 </div>
             `;
@@ -39,7 +46,7 @@ function criarCardInformacaoHTML(data, ordemServico, timestamp) {
 
 
     return `
-        <div class="col-12 py-3 mt-4 shadow rounded-4 d-flex justify-content-center align-content-center">
+        <div class="col-12 py-3 mt-4 shadow rounded-4 d-flex justify-content-center align-content-center mb-5">
             <div class="px-3">
                 <table class="table px-4">
                     <tbody>
@@ -106,7 +113,12 @@ function formatarDataHora(timestamp) {
 
 document.addEventListener("DOMContentLoaded", async () => {
     try {
-        const querySnapshot = await getDocs(collection(db, "registrar"), query => orderBy(query, 'timestamp', 'desc'));
+        const querySnapshot = await getDocs(
+            query(
+                collection(db, "registrar"),
+                orderBy("timestamp", "desc") // Ordenando pelo timestamp em ordem descendente
+            )
+        );
 
         const informacoesDiv = document.getElementById("informacoes");
         informacoesDiv.innerHTML = "";
@@ -122,64 +134,123 @@ document.addEventListener("DOMContentLoaded", async () => {
 });
 
 
+//========================== PESQUISA
 
 
+document.addEventListener("DOMContentLoaded", async () => {
+    // ... (seu código existente)
+  
+    // Referências aos elementos HTML
+    const searchForm = document.getElementById("searchForm");
+    const searchInput = document.getElementById("searchInput");
+    const informacoesDiv = document.getElementById("informacoes");
+  
+    // Função para realizar a pesquisa
+    const realizarPesquisa = async (termo) => {
+      informacoesDiv.innerHTML = ""; // Limpar resultados anteriores
+  
+      try {
+        const querySnapshot = await getDocs(
+          query(
+            collection(db, "registrar"),
+            orderBy("timestamp", "desc")
+          )
+        );
+  
+        querySnapshot.forEach((doc) => {
+          const data = doc.data();
+          const ordemServico = data.ordemServico.toLowerCase(); // Convertendo para minúsculas para pesquisa case-insensitive
+  
+          if (ordemServico.includes(termo.toLowerCase())) {
+            const infoCardHTML = criarCardInformacaoHTML(data, data.ordemServico, data.timestamp);
+            informacoesDiv.innerHTML += infoCardHTML;
+          }
+        });
+  
+        if (informacoesDiv.innerHTML === "") {
+          informacoesDiv.innerHTML = "<p>Nenhum resultado encontrado.</p>";
+        }
+      } catch (error) {
+        console.error("Erro ao obter informações:", error);
+      }
+    };
+  
+    // Manipulador de evento para o formulário de pesquisa
+    searchForm.addEventListener("submit", async (event) => {
+      event.preventDefault(); // Evitar o comportamento padrão de submit
+  
+      const termoPesquisa = searchInput.value.trim(); // Obter o valor do campo de pesquisa
+  
+      if (termoPesquisa !== "") {
+        realizarPesquisa(termoPesquisa); // Executar a função de pesquisa
+      } else {
+        informacoesDiv.innerHTML = ""; // Limpar resultados anteriores
+        // Se o campo de pesquisa estiver vazio, exibir todos os resultados novamente
+  
+        // Chame sua função existente para carregar todos os resultados novamente
+        // Seu código original para exibir todos os resultados após o carregamento do DOM
+        const querySnapshot = await getDocs(
+          query(
+            collection(db, "registrar"),
+            orderBy("timestamp", "desc")
+          )
+        );
+  
+        querySnapshot.forEach((doc) => {
+          const data = doc.data();
+          const infoCardHTML = criarCardInformacaoHTML(data, data.ordemServico, data.timestamp);
+          informacoesDiv.innerHTML += infoCardHTML;
+        });
+      }
+    });
+  
+    // Manipulador de evento para o campo de entrada de pesquisa (detecta a exclusão de conteúdo)
+    searchInput.addEventListener("input", async () => {
+      const termoPesquisa = searchInput.value.trim(); // Obter o valor do campo de pesquisa
+  
+      if (termoPesquisa === "") {
+        informacoesDiv.innerHTML = ""; // Limpar resultados anteriores
+  
+        // Chame sua função existente para carregar todos os resultados novamente
+        // Seu código original para exibir todos os resultados após o carregamento do DOM
+        const querySnapshot = await getDocs(
+          query(
+            collection(db, "registrar"),
+            orderBy("timestamp", "desc")
+          )
+        );
+  
+        querySnapshot.forEach((doc) => {
+          const data = doc.data();
+          const infoCardHTML = criarCardInformacaoHTML(data, data.ordemServico, data.timestamp);
+          informacoesDiv.innerHTML += infoCardHTML;
+        });
+      }
+    });
+  });
+  
 
 
+//   ---------------------   VOLTA AO TOPO
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+document.addEventListener("DOMContentLoaded", function() {
+    const btnVoltarAoTopo = document.getElementById("voltarAoTopoBtn");
+  
+    // Exibir o botão quando a página for rolada para baixo
+    window.addEventListener("scroll", function() {
+      if (window.pageYOffset > 1800) {
+        btnVoltarAoTopo.classList.add("show");
+      } else {
+        btnVoltarAoTopo.classList.remove("show");
+      }
+    });
+  
+    // Função para rolar suavemente até o topo da página ao clicar no botão
+    btnVoltarAoTopo.addEventListener("click", function() {
+      window.scrollTo({
+        top: 0,
+        behavior: "auto"
+      });
+    });
+  });
